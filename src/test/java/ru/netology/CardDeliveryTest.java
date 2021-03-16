559798bef07c37b0cf02e04085667a95cd2e5450
Natalia Marenova <nmarenova@yandex.ru>
2021-03-16 21:28:09 +0300

    Initial commit

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,50 +1,97 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.Duration;
-
-
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.cssSelector;
+import static ru.netology.DataGenerator.*;
 
+class ApplicationFormTestTest {
+    private SelenideElement form;
+    private final String city = getRandomCity();
+    private final String date = getRelevantDate(3);
+    private final String otherDate = getRelevantDate(15);
+    private final String invalidDate = getIrrelevantDate();
+    private final String name = getRandomName();
+    private final String phone = getRandomPhone();
 
-public class CardDeliveryTest {
+    @BeforeAll
+    static void setUpAll() {
+        Configuration.screenshots = false;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
-    void setUp(){
-        open("http://localhost:9999/");
-
+    void setup(){
+        open("http://localhost:9999");
+        form = $("[action]");
+        form.$(cssSelector("[data-test-id=city] input")).sendKeys(city);
+        form.$(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
     }
-
 
     @Test
-    void shouldCardDeliveryNewDate() {
-        User user = DataGenerator.Registration.generateUser();
-        $("[data-test-id='city'] input").setValue(user.getCity());
-        $("[data-test-id= date] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id=date] input").sendKeys(Keys.DELETE);
-        $("[data-test-id= date] input").sendKeys(DataGenerator.Registration.getDate(3));
-        $("[data-test-id=name] input").setValue(user.getName());
-        $("[data-test-id=phone] input").setValue(user.getPhone());
-        $("[data-test-id=agreement]").click();
-        $$("button").find(exactText("Запланировать")).click();
-        $(" [data-test-id='success-notification']").shouldBe(visible, Duration.ofMillis(15000))
-                .shouldHave(text("Успешно! Встреча успешно запланирована на " + DataGenerator.Registration.getDate(3)));
-        $(".calendar-input input").sendKeys(Keys.CONTROL + "a");
-        $(".calendar-input input").sendKeys(Keys.DELETE);
-        $(".calendar-input input").sendKeys(DataGenerator.Registration.getDate(3));
-        $$("button").find(exactText("Запланировать")).click();
-        $(" [data-test-id='replan-notification']")
-                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
-        $$("button").find(exactText("Перепланировать")).click();
-        $(" [data-test-id='success-notification']")
-                .shouldHave(text("Успешно! Встреча успешно запланирована на " + DataGenerator.Registration.getDate(3)));
+    void shouldSubmitRequest() {
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(date);
+        form.$(cssSelector("[name=name]")).sendKeys(name);
+        form.$(cssSelector("[name=phone]")).sendKeys(phone);
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Запланировать")).click();
+        $(cssSelector(".notification__content")).waitUntil(Condition.visible, 15000).shouldHave(text(date));
+    }
+
+    @Test
+    void shouldGiveNewDateWhenSameRequest() {
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(date);
+        form.$(cssSelector("[name=name]")).sendKeys(name);
+        form.$(cssSelector("[name=phone]")).sendKeys(phone);
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Запланировать")).click();
+        $(cssSelector(".notification__content")).waitUntil(Condition.visible, 15000).shouldHave(text(date));
+        open("http://localhost:9999");
+        form.$(cssSelector("[data-test-id=city] input")).sendKeys(city);
+        form.$(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(otherDate);
+        form.$(cssSelector("[name=name]")).sendKeys(name);
+        form.$(cssSelector("[name=phone]")).sendKeys(phone);
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Запланировать")).click();
+        $(cssSelector(".notification_status_error .button")).click();
+        $(cssSelector(".notification__content")).waitUntil(Condition.visible, 15000).shouldHave(text(otherDate));
+    }
+
+    @Test
+    void shouldNotGiveNewDateWhenInvalidDate() {
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(date);
+        form.$(cssSelector("[name=name]")).sendKeys(name);
+        form.$(cssSelector("[name=phone]")).sendKeys(phone);
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Запланировать")).click();
+        $(cssSelector(".notification__content")).waitUntil(Condition.visible, 15000).shouldHave(text(date));
+        open("http://localhost:9999");
+        form.$(cssSelector("[data-test-id=city] input")).sendKeys(city);
+        form.$(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(invalidDate);
+        form.$(cssSelector("[name=name]")).sendKeys(name);
+        form.$(cssSelector("[name=phone]")).sendKeys(phone);
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Запланировать")).click();
+        $(byText("Заказ на выбранную дату невозможен")).waitUntil(Condition.visible, 15000);
     }
 }
-
-
--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,29 +1,44 @@
 package ru.netology;
 
 import com.github.javafaker.Faker;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 
+@Data
+@NoArgsConstructor
 public class DataGenerator {
-    private DataGenerator() {
+
+    public static String getRandomCity() {
+        List<String> list = Arrays.asList("Новосибирск", "Москва", "Санкт-Петербург", "Казань", "Йошкар-Ола");
+        Random rand = new Random();
+        String randomElement = list.get(rand.nextInt(list.size()));
+        return randomElement;
     }
 
-    public static class Registration {
-        private Registration() {
-        }
+    public static String getRelevantDate(int days) {
+        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
 
-        public static User generateUser() {
-            Faker faker = new Faker(new Locale("ru"));
-            return new User(faker.address().city(), faker.name().fullName(), faker.phoneNumber().phoneNumber());
-        }
+    public static String getIrrelevantDate() {
+        String today = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return today;
+    }
 
-        public static String getDate(int date) {
-            LocalDate today = LocalDate.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-            LocalDate newDate=today.plusDays(date);
-            return formatter.format(newDate);
-        }
+    public static String getRandomName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    public static String getRandomPhone() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
     }
 }
